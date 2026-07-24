fac2382ebf38ca89901d9b74774fb8e88fb8f320
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-07-24 10:48:05 -0300

    feat(types/auth.d.ts): add CLIENT role to NextAuth types

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -4,19 +4,19 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role: "ADMIN" | "EMPLOYEE";
+      role: "ADMIN" | "EMPLOYEE" | "CLIENT";
     } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
-    role: "ADMIN" | "EMPLOYEE";
+    role: "ADMIN" | "EMPLOYEE" | "CLIENT";
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
-    role: "ADMIN" | "EMPLOYEE";
+    role: "ADMIN" | "EMPLOYEE" | "CLIENT";
   }
 }
