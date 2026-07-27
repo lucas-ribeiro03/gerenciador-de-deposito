db43465c8e811164b4b1ad38c5ab6201dcdf9ec4
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-07-27 11:41:54 -0300

    feat: add user seed file

--- prisma/seeds/user-seed.ts
+++ prisma/seeds/user-seed.ts
@@ -1,11 +1,23 @@
 import { prisma } from "@/prisma";
 import { hashPassword } from "@/lib/auth/hash-password";
-await prisma.user.create({
-  data: {
-    name: process.env.ADMIN_NAME!,
-    email: process.env.ADMIN_EMAIL!,
-    phone: process.env.ADMIN_PHONE!,
-    password: await hashPassword(process.env.ADMIN_PASSWORD!),
-    role: "ADMIN",
-  },
-});
+export async function seedUsers() {
+  const admin = await prisma.user.findUnique({
+    where: {
+      email: "admin@pointdogrell.com",
+    },
+  });
+
+  if (admin) {
+    console.log("✔ Usuário administrador já existe.");
+    return;
+  }
+  await prisma.user.create({
+    data: {
+      name: process.env.ADMIN_NAME!,
+      email: process.env.ADMIN_EMAIL!,
+      phone: process.env.ADMIN_PHONE!,
+      password: await hashPassword(process.env.ADMIN_PASSWORD!),
+      role: "ADMIN",
+    },
+  });
+}
