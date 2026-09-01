c3f40dc235ebb14751f7596c8c359c3e2300119b
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-09-01 09:55:45 -0300

    feat(navbar): enhance UserDropdown with profile dialog and orders link

--- components/navbar/user-dropdown.tsx
+++ components/navbar/user-dropdown.tsx
@@ -15,55 +15,82 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import { ProfileDialog } from "./profile/profile-dialog";
+import { useRouter } from "next/navigation";
 
-export function UserDropdown() {
+type UserDropdownProps = {
+  user: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+};
+
+export function UserDropdown({ user }: UserDropdownProps) {
   const [_, startTransition] = useTransition();
   function logout() {
     startTransition(async () => {
       await logoutAction();
     });
   }
+
+  const router = useRouter();
+
+  const [profileOpen, setProfileOpen] = useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarFallback>
-            <User className="size-5" />
-          </AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarFallback>
+              <User className="size-5" />
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent
-        align="center"
-        className="w-56 rounded-xl border border-border bg-popover backdrop-blur-xl shadow-xl text-popover-foreground"
-      >
-        <DropdownMenuGroup>
-          <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
-
-          <DropdownMenuSeparator className="w-full h-[0.5px] bg-border" />
-
-          <DropdownMenuItem>
-            <User className="mr-2 size-4" />
-            Meu Perfil
-          </DropdownMenuItem>
-
-          <DropdownMenuItem>
-            <Package className="mr-2 size-4" />
-            Meus Pedidos
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-
-        <DropdownMenuSeparator />
-
-        <DropdownMenuItem
-          onClick={logout}
-          className="text-destructive focus:text-destructive"
+        <DropdownMenuContent
+          align="center"
+          className="w-56 rounded-xl border border-border bg-popover backdrop-blur-xl shadow-xl text-popover-foreground"
         >
-          <LogOut className="mr-2 size-4" />
-          Sair
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+
+            <DropdownMenuSeparator className="w-full h-[0.5px] bg-border" />
+
+            <DropdownMenuItem onClick={() => setProfileOpen(true)}>
+              <User className="mr-2 size-4" />
+              Meu Perfil
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => {
+                router.push("/orders");
+              }}
+            >
+              <Package className="mr-2 size-4" />
+              Meus Pedidos
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+            onClick={logout}
+            className="text-destructive focus:text-destructive"
+          >
+            <LogOut className="mr-2 size-4" />
+            Sair
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <ProfileDialog
+        open={profileOpen}
+        onOpenChange={setProfileOpen}
+        user={user}
+      />
+    </>
   );
 }
